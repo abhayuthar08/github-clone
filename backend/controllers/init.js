f609b4aafb26089208f637158c240b7214aabc82
Abhay Suthar <abhaysuthar6355@gmail.com>
2025-05-01 21:52:37 +0530

    base setup and feature init command

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,21 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 async function initRepo() {
-    console.log("Init command called");
+    const repoPath = path.resolve(process.cwd() , "apnaGit");
+    const commitPath = path.join(repoPath , "commits");
+
+    try {
+        await fs.mkdir(repoPath,{recursive : true});
+        await fs.mkdir(commitPath,{recursive : true});
+        await fs.writeFile(
+            path.join(repoPath, "config.json"),
+            JSON.stringify({bucket: process.env.S3_BUCKET})
+        );
+        console.log("Reposatory Initialized!");
+    } catch (error) {
+        console.error("Error initializing the repsatory..." , error);
+    }
 }
 
 module.exports = { initRepo };
